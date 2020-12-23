80062e2f6df3cf86089eb096868c2a25c7d76a3b
DESKTOP-CBA7E8M\ACER <cvetanovic9696@gmail.com>
2020-12-23 22:54:42 +0100

    fix

--- mobile-api/src/main/java/com/project/mobileapi/MobileApiApplication.java
+++ mobile-api/src/main/java/com/project/mobileapi/MobileApiApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@SpringBootApplication( scanBasePackages = {"com.project"})
+@SpringBootApplication
 public class MobileApiApplication {
 
     public static void main(String[] args) {
