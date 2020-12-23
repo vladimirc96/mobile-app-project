01db62f88b8bea78d058cbcfd7a0e0c62b71e464
DESKTOP-CBA7E8M\ACER <cvetanovic9696@gmail.com>
2020-12-23 21:08:22 +0100

    backend added

---
+++ mobile-api/src/main/java/com/project/mobileapi/MobileApiApplication.java
@@ -0,0 +1,13 @@
+package com.project.mobileapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MobileApiApplication.class, args);
+    }
+
+}
