819167e62fae8fb455752e72eecebfbf52726084
Duca <k.dule96@gmail.com>
2021-01-21 23:05:50 +0100

    Additional ad datails implemented

--- react_setup/AwesomeProject/Ad.js
+++ react_setup/AwesomeProject/Ad.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { ImageBackground, StyleSheet, TouchableOpacity, Text, View, Image, ActivityIndicator } from 'react-native';
 import * as Font from 'expo-font';
 import {Octicons} from '@expo/vector-icons'
+import { FontAwesome } from '@expo/vector-icons';
 
 const customFonts = {
   'Comfortaa-Bold': require('./assets/fonts/Comfortaa-Bold.ttf')
@@ -44,17 +45,49 @@ export default class Ad extends React.Component {
             </View>
             <View style = {styles.adMainText}>
                 <Text style = {styles.adTitle}>{this.props.title}</Text>
-                <View style={{flexDirection:'row'}}>
+                <View style={styles.ownerContainer}>
+                  <View style={styles.leftDashContainer}>
                     <Octicons name='dash' style={styles.line}/>
                     <Octicons name='dash' style={styles.line}/>
-                    <Text style={styles.ownerName}> Slobodanka Jakovljevic </Text>
+                    <Octicons name='dash' style={styles.line}/>
+                    <Octicons name='dash' style={styles.line}/>
+                  </View>
                 </View>
                 <View style={styles.description}>
                     <Text style={styles.descriptionText}> -svi zanrovi -svi zanrovi -svi zanrovi -svi zanrovi -svi zanrovi -svi</Text>
                 </View>
+                <View style={styles.ownerNameContainer}>
+                    <Text style={styles.ownerName}>Vlasnik: Slobodanka Jakovljevic </Text>
+                </View>
             </View>
             <View style={styles.adDetails}>
-
+              <View style = {styles.priceContainer}>
+                <Text style={styles.priceValue}>Dogovor</Text>
+              </View>
+              <View style = {styles.publishDateContainer}>
+                <Text style={styles.publishTitle}>postavljen</Text>
+                <Text style={styles.publishDate}>danas</Text>
+              </View>
+              <View style = {styles.location}>
+                <Text style = {styles.locationText}>Lokacija</Text>
+              </View>
+              <View>
+                <View style = {styles.ratingValue}>
+                  <Text style = {styles.ratingValueText}>ocena </Text>
+                  <Text style = {styles.ratingValueText}>3.5 </Text>
+                </View>
+                <View style = {styles.ratingStars}>
+                  <FontAwesome name="star" style={styles.star} />
+                  <FontAwesome name="star" style={styles.star} />
+                  <FontAwesome name="star" style={styles.star} />
+                  <FontAwesome name="star-half-empty" style={styles.star} />
+                  <FontAwesome name="star-o" style={styles.star}/>
+                </View>
+                <View style = {styles.ratingComment}>
+                  <Text style = {styles.ratingCommentText}>od strane </Text>
+                  <Text style = {styles.ratingCommentText}>4 korisnika </Text>
+                </View>
+              </View>
             </View>
         </View>
         </TouchableOpacity>
@@ -99,7 +132,7 @@ const styles = StyleSheet.create({
       marginTop: 5,
       width: 75,
       height: 100,
-      backgroundColor: 'blue',
+      backgroundColor: '#1e1c24',
       marginLeft: 5
   },
   line:{
@@ -118,8 +151,87 @@ const styles = StyleSheet.create({
       fontSize: 10,
       color: '#ededed'
   },
+  ownerContainer: {
+    flexDirection:'row',
+  },
+  leftDashContainer: {
+    marginLeft: 1,
+    flexDirection: 'row'
+  },
+  ownerNameContainer: {
+    alignSelf: 'flex-end'
+  },
   ownerName: {
-    fontSize: 9,
+    fontSize: 8,
+    color: '#ededed'
+  },
+  priceContainer: {
+    alignSelf: 'center',
+    height: 18,
+    width: 65,
+    backgroundColor: 'black'
+  },
+  priceValue: {
+    textAlign: 'center',
+    marginTop: 2,
+    fontSize: 10,
+    color: '#ededed'
+  },
+  publishDateContainer: {
+    backgroundColor:'#1e1c24',
+    alignContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+    width: 60,
+    marginTop: 4
+  },
+  publishTitle: {
+    fontSize: 8,
+    color: '#ededed'
+  },
+  publishDate: {
+    fontSize: 8,
+    color: '#ededed'
+  },
+  location: {
+    backgroundColor:'#1e1c24',
+    alignContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+    width: 60,
+    marginTop: 5
+  },
+  locationText: {
+    fontSize: 8,
+    color: '#ededed'
+  },
+  ratingValue: {
+    flexDirection: 'row',
+    width: 75,
+    justifyContent: 'center',
+    marginTop: 3
+  },
+  ratingValueText: {
+    fontSize: 8,
+    color: '#ededed'
+  },
+  ratingStars: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    width: 75,
+    marginTop: 1
+  },
+  star: {
+    color: "#ededed"
+  },
+  ratingComment: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    width: 75,
+    marginTop: 1
+  },
+  ratingCommentText: {
+    fontSize: 7,
     color: '#ededed'
   }
   });
