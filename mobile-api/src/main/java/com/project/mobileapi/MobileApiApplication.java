704ea91b6495490e1429f89ceccede881de2f9a1
DESKTOP-CBA7E8M\ACER <cvetanovic9696@gmail.com>
2021-02-08 00:07:18 +0100

    cors filter added

--- mobile-api/src/main/java/com/project/mobileapi/MobileApiApplication.java
+++ mobile-api/src/main/java/com/project/mobileapi/MobileApiApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = { SecurityAutoConfiguration.class })
@@ -11,6 +13,16 @@ public class MobileApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MobileApiApplication.class, args);
+    }
+
+    @Bean
+    public FilterRegistrationBean corsFilterRegistration() {
+        FilterRegistrationBean registrationBean =
+                new FilterRegistrationBean(new CORSFilter());
+        registrationBean.setName("CORS Filter");
+        registrationBean.addUrlPatterns("/*");
+        registrationBean.setOrder(1);
+        return registrationBean;
     }
 
 }
--- mobile-api/src/main/java/com/project/mobileapi/util/KeyValue.java
+++ mobile-api/src/main/java/com/project/mobileapi/util/KeyValue.java
@@ -14,4 +14,5 @@ public class KeyValue {
     private Long id;
     private String value;
 
+
 }
