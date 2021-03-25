b3b34cd3e77b4d5961af3606a22240e799d26ac5
marko996 <marko.zlatanovic996@gmail.com>
2021-03-25 22:42:05 +0100

    Created modal

--- react_setup/AwesomeProject/screens/Ads.js
+++ react_setup/AwesomeProject/screens/Ads.js
@@ -1,46 +1,340 @@
 import React from "react";
-import { ImageBackground, Text, View, ScrollView } from "react-native";
+import { ImageBackground, Text, View, ScrollView, Modal, TouchableOpacity, Image, StyleSheet } from "react-native";
 import Ad from "../components/Ad";
 import { adsStyles } from "../shared/Styles";
+import { Ionicons } from '@expo/vector-icons';
+import {
+  Octicons,
+  Fontisto,
+  FontAwesome,
+  SimpleLineIcons,
+  Feather,
+} from "@expo/vector-icons";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
+import { BlurView } from 'expo-blur';
+import { Dimensions } from "react-native";
+
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
 
 export default class Ads extends React.Component {
+  constructor(){
+    super()
+    this.state ={
+      showModal: false
+    }
+  }
+
+
   render() {
     const backgroundImage = require("./../assets/images/logInBackground.jpg");
+    const avatar = require("./../assets/images/gitara.jpg");
     return (
       <ImageBackground
         style={adsStyles.backgroundImageContainer}
         source={backgroundImage}
       >
+      {this.state.showModal ?
+        <BlurView intensity={100} tint={"dark"} style={{width:wp("100%"), height:hp("100%")}}>
+        <ScrollView>
+          <View style={adsStyles.mainContainer}>
+                    <View style={{alignSelf: "center", marginTop: hp("1%")}}>
+                      <Modal
+                        transparent={true}
+                        visible={this.state.showModal}
+                        style={{borderWidth:0,borderColor:"none"}}
+                        >
+                          <View style={{backgroundColor:"white", width:wp("90%"), height: hp("85%"), borderRadius: 5}}>
+                            <View style={{ flexDirection: "row",justifyContent:"space-between"}}>
+                              <View></View>
+                              <Ionicons name="md-close" size={26} onPress={() => this.setState({showModal: false})} style={{marginTop:hp("0.5%"), marginRight:hp("0.5%")}} color="black" />
+                            </View>
+                            <View style={{alignSelf:"center"}}>
+
+                              <View style={{borderColor:"black", borderWidth: 1, borderRadius: 10, width: wp("86%"), height: hp("5%"), marginLeft: wp("2.5%"), marginRight: wp("2.5%"), textAlign:"center"}}> 
+                                <Text style={{fontSize: hp("3%"), textAlign:"center"}}> Casovi nemackog </Text>
+                              </View>
+
+
+                              <View style={styles.basicUserInfo}>
+                                  <Image style={
+                                        windowHeight * 0.37 < windowWidth * 0.7
+                                          ? styles.profileImageHeight
+                                          : styles.profileImageWidth
+                                      } source={avatar} />
+                                <Text style={styles.profileName}>Vladimir Cvetanovic</Text>
+                                <View style={styles.userLocation}>
+                                  <SimpleLineIcons name="location-pin" size={hp("2.5%")} color="black" />
+                                  <Text style={styles.location}>Novi Sad, Srbija</Text>
+                                </View>
+                                <View style={styles.userMail}>
+                                  <Fontisto name="email" size={hp("2.5%")} color="black" />
+                                  <Text style={styles.location}>dovla.car@gmail.com</Text>
+                                </View>
+                                <View style={styles.userOntact}>
+                                  <Feather name="phone" size={hp("2.5%")} color="black" />
+                                  <Text style={styles.location}>+381 62 266 021</Text>
+                                </View>
+                                <View style={styles.userRating}>
+                                  <TouchableOpacity style={styles.likeComponent}>
+                                    <SimpleLineIcons name="like" style={styles.like} />
+                                  </TouchableOpacity>
+                                  <Text style={styles.ratingText}>6969</Text>
+                                  <TouchableOpacity style={styles.dislikeComponent}>
+                                    <SimpleLineIcons name="dislike" style={styles.dislike} />
+                                  </TouchableOpacity>
+                                  <Text style={styles.ratingText}>69</Text>
+                                </View>
+                              </View>
+                              <View style={styles.description}>
+                                <Text style={{marginLeft: 2, marginRight: 2}}>
+                                  Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text
+                                  Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text
+                                  Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text
+                                  Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text
+                                </Text>
+                              </View>
+                            </View>
+                          </View>
+                        </Modal>
+                    </View>
+          </View>
+        </ScrollView>
+        </BlurView>
+        :
         <ScrollView>
           <View style={adsStyles.mainContainer}>
             <View style={adsStyles.titleContainer}>
               <Text style={adsStyles.titleText}> Privatni casovi </Text>
             </View>
-            <View style={adsStyles.numberOfAdsContainer}></View>
-            <View style={adsStyles.adContainer}>
-              <Ad
-                navigation={this.props.navigation}
-                title="CASOVI GITARE DUZI NASLOV"
-              />
-            </View>
-            <View style={adsStyles.adContainer}>
-              <Ad title="CASOVI GITARE" />
-            </View>
-            <View style={adsStyles.adContainer}>
-              <Ad title="Casovi gitare" />
-            </View>
-            <View style={adsStyles.adContainer}>
-              <Ad title="Casovi gitare" />
-            </View>
-            <View style={adsStyles.adContainer}>
-              <Ad title="Casovi gitare" />
-            </View>
-            <View style={adsStyles.adContainer}>
-              <Ad title="Casovi gitare" />
-            </View>
+                <View>
+                  <View style={adsStyles.numberOfAdsContainer}></View>
+                <View style={adsStyles.adContainer}>
+                  <Ad
+                    navigation={this.props.navigation}
+                    title="CASOVI GITARE DUZI NASLOV"
+                    onPress={() => this.setState({showModal: true})}
+                  />
+                </View>
+                <View style={adsStyles.adContainer}>
+                  <Ad title="CASOVI GITARE" />
+                </View>
+                <View style={adsStyles.adContainer}>
+                  <Ad title="Casovi gitare" />
+                </View>
+                <View style={adsStyles.adContainer}>
+                  <Ad title="Casovi gitare" />
+                </View>
+                <View style={adsStyles.adContainer}>
+                  <Ad title="Casovi gitare" />
+                </View>
+                <View style={adsStyles.adContainer}>
+                  <Ad title="Casovi gitare" />
+                </View>
+                </View>
           </View>
         </ScrollView>
+      }
       </ImageBackground>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  backgroundImageContainer: {
+    flex: 1,
+    resizeMode: "cover",
+  },
+  mainContainer: {
+    alignSelf: "center",
+    marginTop: hp("3%"),
+    width: wp("90%"),
+    height: wp("60%")
+  },
+  basicUserInfo: {
+    maxHeight: hp("50%"),
+    marginBottom: wp("1%"),
+    marginTop: hp("1%"),
+    marginLeft: wp("2.5%"),
+    marginRight: wp("2.5%"),
+    backgroundColor: "white",
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "black"
+  },
+  description: {
+    minHeight: hp("33%"),
+    marginBottom: hp("0.5%"),
+    marginTop: hp("1%"),
+    marginLeft: wp("2.5%"),
+    marginRight: wp("2.5%"),
+    paddingLeft: wp("2%"),
+    paddingLeft: wp("1%"),
+    paddingTop: wp("2%"),
+    paddingBottom: wp("2%"),
+    backgroundColor: "white",
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "black"
+  },
+  profileImageHeight: {
+    alignSelf: "center",
+    marginTop: hp("1.5%"),
+    width: hp("15%"),
+    height: hp("15%"),
+  },
+  profileImageWidth:{
+    alignSelf: "center",
+    marginTop: wp("3%"),
+    width: wp("30%"),
+    height: wp("30%"),
+  },
+  titleIconWidth: {
+    width: wp("12%"),
+    height: wp("12%"),
+    top: wp("4%"),
+    left: wp("2.5%"),
+  },
+  profileName: {
+    alignSelf: "center",
+    marginTop: hp("1.5%"),
+    marginBottom: hp("0.5%"),
+    fontSize: hp("3%"),
+    fontWeight: "bold",
+    color: "black",
+  },
+  userLocation: {
+    flexDirection: "row",
+    alignSelf: "center",
+    marginBottom: hp("0.25%"),
+  },
+  location: {
+    marginLeft: wp("1%"),
+    fontSize: wp("4%"),
+    color: "black",
+  },
+  userMail: {
+    flexDirection: "row",
+    alignSelf: "center",
+    marginBottom: hp("0.5%"),
+  },
+  userOntact: {
+    flexDirection: "row",
+    alignSelf: "center",
+    marginBottom: hp("0.5%"),
+  },
+  userRating: {
+    flexDirection: "row",
+    alignSelf: "center",
+    marginBottom: hp("1.5%"),
+  },
+  ratingText: {
+    fontSize: hp("2%"),
+    color: "black",
+    marginLeft: wp("1%"),
+    marginRight: wp("3%"),
+  },
+  likeComponent: {
+    color: "black",
+  },
+  like: {
+    fontSize: hp("2.75%"),
+    color: "black"
+  },
+  dislikeComponent: {
+    color: "black",
+  },
+  dislike: {
+    fontSize: hp("2.75%"),
+    color: "black",
+  },
+  userContact: {
+    flexDirection: "row",
+    alignSelf: "center",
+  },
+  userEmail: {
+    flexDirection: "row",
+    alignSelf: "center",
+  },
+  aboutUser: {
+    marginBottom: hp("0.5%"),
+    backgroundColor: "#1e1c24",
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "black",
+  },
+  smallContainer: {
+    marginBottom: hp("0.5%"),
+    backgroundColor: "#1e1c24",
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "#ededed",
+  },
+  sectionName: {
+    left: wp("2%"),
+    marginTop: hp("1.5%"),
+    marginBottom: hp("1.5%"),
+    marginLeft: wp("2%"),
+    marginRight: wp("2%"),
+    fontSize: hp("2.5%"),
+    color: "#ededed",
+  },
+  dashContainer: {
+    marginLeft: wp("0.3%"),
+    flexDirection: "row",
+  },
+  line: {
+    textAlignVertical: "center",
+    fontSize: hp("3%"),
+    color: "#ededed",
+  },
+  userDetails: {
+    paddingHorizontal: hp("1.25%"),
+    paddingVertical: wp("2%"),
+    marginHorizontal: wp("1.5%"),
+    marginVertical: hp("0.5%"),
+    borderRadius: 10,
+    backgroundColor: "#1f1f2f",
+  },
+  details: {
+    fontSize: hp("2%"),
+    color: "#ededed",
+  },
+  commentTitle:{
+    fontSize: hp("2%"),
+    fontWeight: "bold",
+    color: "#ededed",
+  },
+  commentUser:{
+    fontSize: hp("2%"),
+    fontWeight: "bold",
+    marginTop: hp("0.5%"),
+    marginLeft: wp("2%"),
+    color: "#ededed",
+  },
+  commentTime:{
+    fontSize: hp("2%"),
+    marginTop: hp("0.5%"),
+    marginLeft: wp("1%"),
+    color: "#ededed",
+  },
+  commentText:{
+    fontSize: hp("2%"),
+    marginTop: hp("1%"),
+    color: "#ededed",
+  },
+  arrow: {
+    alignSelf: "center",
+    fontSize: hp("3%"),
+    color: "#ededed",
+  },
+  arrowSmall: {
+    alignSelf: "center",
+    marginTop: hp("0.5%"),
+    fontSize: hp("3%"),
+    color: "#ededed",
+  }
+});
