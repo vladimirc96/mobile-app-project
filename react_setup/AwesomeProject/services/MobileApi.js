d9c23a8c2cc0d461bb2e2a7ea5bcaba9210265c2
DESKTOP-CBA7E8M\ACER <cvetanovic9696@gmail.com>
2021-05-25 21:15:45 +0200

    Remove

--- react_setup/AwesomeProject/services/MobileApi.js
+++ react_setup/AwesomeProject/services/MobileApi.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 import Constants from "expo-constants";
 import persistedStore from "../store/store";
-import { showPopup, customButton } from "../shared/AlertPopup";
-import { logout } from "../store/actions/authentication/authenticationActions";
-import jwt_decode from "jwt-decode";
 
 const { manifest } = Constants;
 
@@ -16,27 +13,9 @@ const Axios = axios.create({
   baseURL: "http://".concat(api),
 });
 
-const redirectLogin = () => {
-  persistedStore.dispatch(logout());
-};
-
 Axios.interceptors.request.use((req) => {
   const state = persistedStore.store.getState();
   const token = state.authenticationReducer.token;
-
-  // const decoded = jwt_decode(token);
-  // const dateNow = new Date();
-
-  // if (decoded.exp < dateNow.getTime()) {
-  //   const buttons = [customButton("Prijavi se", redirectLogin)];
-  //   showPopup(
-  //     "",
-  //     "Sesija vam je istekla. Potrebno je da se prijavite ponovo kako bi mogli da nastavite korišćenje aplikacije.",
-  //     buttons
-  //   );
-  //   return;
-  // }
-
   if (token !== null && token !== undefined) {
     req.headers.Authorization = "Bearer " + token.accessToken;
   }
